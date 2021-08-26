e7b34b2a8f3de5cbb4f2f3ac1878c19f32a7ddc8
Paul Allcca Flores <paul.flores@upb.edu.co>
2021-08-26 22:48:28 +0000

    add var: Una dirección de memoria

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+
+    printf("Sistemas Operativos\n");
+
+    //var es una dirección de memoria donde voy almacenar enteros
+    int var =10;
+
+    printf("var: %d\n", var);
+
+    //quiero una variabkle que almacene la dirección de memoria de una
+    //variable que almacena enteros.
+    //PUNTERO: es una variable que almacena direcciones.
+    return EXIT_SUCCESS;
+}
